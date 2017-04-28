a51a73c6a6e55dcf4830f01cbd1e6a486a00719a
FS <63419182@163.com>
2017-04-28 09:50:35 +0800

    First Commit

---
+++ Util_DownloadZip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Util_DownloadZip : MonoBehaviour
+{
+	public void DownloadTo(string url, string targetDirectory, Action<string> failedDel, Action completeDel)
+	{
+		StartCoroutine(DownloadURL(url, targetDirectory, failedDel, completeDel));
+	}
+	private IEnumerator DownloadURL(string url, string targetDirectory, Action<string> failedDel, Action completeDel)
+	{
+		WWW www = new WWW(url);
+		yield return www;
+		if (www.error == null)
+		{
+			try
+			{
+				Util_Zip.ExtractZip(new MemoryStream(www.bytes), targetDirectory);
+				if (completeDel != null)
+				{
+					completeDel();
+				}
+			}
+			catch (Exception ex)
+			{
+				if (failedDel != null)
+				{
+					failedDel("Util_Zip.ExtractZip error:" + ex.Message);
+				}
+			}
+		}
+		else
+		{
+			if (failedDel != null)
+			{
+				failedDel(www.error + "\r\n" + url);
+			}
+		}
+	}
+}
---
+++ Util_Zip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public static class Util_Zip
+{
+	private static string Pd = "^$*&(!";
+	public static string CreateZip(string zipPath, string dirPath)
+	{
+		FastZip zip = new FastZip();
+		zip.Password = Pd;
+		zip.CreateZip(zipPath, dirPath, true, ".*\\.(bytes)$");
+		return zipPath;
+	}
+	public static string ExtractZip(string zipPath, string dirPath)
+	{
+		FastZip zip = new FastZip();
+		zip.Password = Pd;
+		zip.ExtractZip(zipPath, dirPath, "");
+		return dirPath;
+	}
+	public static string ExtractZip(Stream zipStream, string dirPath)
+	{
+		FastZip zip = new FastZip();
+		zip.Password = Pd;
+		zip.ExtractZip(zipStream, dirPath, FastZip.Overwrite.Always, null, "", "", false, true);
+		return dirPath;
+	}
+}
